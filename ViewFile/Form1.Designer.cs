ef90a3fb73f59b7c303b5822605d6acbcd8883e5
student <soloxpwnz@mail.ru>
2020-09-11 18:48:25 +0300

    1

--- ViewFile/Form1.Designer.cs
+++ ViewFile/Form1.Designer.cs
@@ -28,80 +28,83 @@ namespace ViewFile
         /// </summary>
         private void InitializeComponent()
         {
-            this.locationTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.displayTextBox = new System.Windows.Forms.TextBox();
-            this.browseButton = new System.Windows.Forms.Button();
-            this.showButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // locationTextBox
-            // 
-            this.locationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.locationTextBox.Location = new System.Drawing.Point(70, 12);
-            this.locationTextBox.Name = "locationTextBox";
-            this.locationTextBox.Size = new System.Drawing.Size(305, 20);
-            this.locationTextBox.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Location:";
-            // 
-            // displayTextBox
-            // 
-            this.displayTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.displayTextBox.Location = new System.Drawing.Point(12, 38);
-            this.displayTextBox.Multiline = true;
-            this.displayTextBox.Name = "displayTextBox";
-            this.displayTextBox.ReadOnly = true;
-            this.displayTextBox.Size = new System.Drawing.Size(535, 432);
-            this.displayTextBox.TabIndex = 2;
-            // 
-            // browseButton
-            // 
-            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.browseButton.Location = new System.Drawing.Point(381, 13);
-            this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(84, 23);
-            this.browseButton.TabIndex = 3;
-            this.browseButton.Text = "Browse";
-            this.browseButton.UseVisualStyleBackColor = true;
-            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
-            // 
-            // showButton
-            // 
-            this.showButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.showButton.Location = new System.Drawing.Point(471, 12);
-            this.showButton.Name = "showButton";
-            this.showButton.Size = new System.Drawing.Size(75, 23);
-            this.showButton.TabIndex = 4;
-            this.showButton.Text = "View file";
-            this.showButton.UseVisualStyleBackColor = true;
-            this.showButton.Click += new System.EventHandler(this.showButton_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 482);
-            this.Controls.Add(this.showButton);
-            this.Controls.Add(this.browseButton);
-            this.Controls.Add(this.displayTextBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.locationTextBox);
-            this.MinimumSize = new System.Drawing.Size(250, 150);
-            this.Name = "Form1";
-            this.Text = "View File";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.locationTextBox = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.displayTextBox = new System.Windows.Forms.TextBox();
+      this.browseButton = new System.Windows.Forms.Button();
+      this.showButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // locationTextBox
+      // 
+      this.locationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.locationTextBox.Location = new System.Drawing.Point(70, 13);
+      this.locationTextBox.Name = "locationTextBox";
+      this.locationTextBox.Size = new System.Drawing.Size(305, 20);
+      this.locationTextBox.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 16);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(51, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Location:";
+      // 
+      // displayTextBox
+      // 
+      this.displayTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.displayTextBox.BackColor = System.Drawing.SystemColors.Window;
+      this.displayTextBox.Location = new System.Drawing.Point(12, 38);
+      this.displayTextBox.Multiline = true;
+      this.displayTextBox.Name = "displayTextBox";
+      this.displayTextBox.ReadOnly = true;
+      this.displayTextBox.Size = new System.Drawing.Size(535, 432);
+      this.displayTextBox.TabIndex = 2;
+      // 
+      // browseButton
+      // 
+      this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.browseButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.browseButton.Location = new System.Drawing.Point(381, 11);
+      this.browseButton.Name = "browseButton";
+      this.browseButton.Size = new System.Drawing.Size(84, 23);
+      this.browseButton.TabIndex = 3;
+      this.browseButton.Text = "Browse";
+      this.browseButton.UseVisualStyleBackColor = false;
+      this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+      // 
+      // showButton
+      // 
+      this.showButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.showButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.showButton.Location = new System.Drawing.Point(471, 10);
+      this.showButton.Name = "showButton";
+      this.showButton.Size = new System.Drawing.Size(75, 23);
+      this.showButton.TabIndex = 4;
+      this.showButton.Text = "View file";
+      this.showButton.UseVisualStyleBackColor = false;
+      this.showButton.Click += new System.EventHandler(this.showButton_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(559, 482);
+      this.Controls.Add(this.showButton);
+      this.Controls.Add(this.browseButton);
+      this.Controls.Add(this.displayTextBox);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.locationTextBox);
+      this.MinimumSize = new System.Drawing.Size(250, 150);
+      this.Name = "Form1";
+      this.Text = "View File";
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
